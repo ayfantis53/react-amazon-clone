6ce24f0bb37c53001d09ce91bf54c1d1a96e7874
Andrew Yfantis <andrew_yfantis@yahoo.com>
2022-06-11 15:54:02 -0700

    working tests

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,21 +1,15 @@
 import { render, screen } from "@testing-library/react";
 import App from '../App';
 
-/*  describe('App component', () => {
+describe('App component', () => {
   test('renders the landing page', () => {
-    render(<App />);
+    render(App);
   });
- });  */
 
- describe('true is truthy and false is falsy', () => {
-  test('true is truthy', () => {
-    expect(true).toBe(true);
-  });
- 
   test('false is falsy', () => {
     expect(false).toBe(false);
   });
- });
+ }); 
 
 
   
